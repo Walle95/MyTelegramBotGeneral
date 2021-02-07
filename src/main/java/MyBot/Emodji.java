b067fdb09c082ea544b2fe2acdd4e4bea4f4dc65
Walle95 <valentinshabanov95@gmail.com>
2021-02-07 20:23:54 +0700

    Initial commit

---
+++ src/main/java/MyBot/Emodji.java
@@ -0,0 +1,9 @@
+package MyBot;
+
+import com.vdurmont.emoji.EmojiParser;
+
+public class Emodji {
+    //вставим эмодзи
+   String emodjiPhone = EmojiParser.parseToUnicode(":phone:");
+   String emodjiEmail = EmojiParser.parseToUnicode(":email:");
+}
---
+++ src/main/java/MyBot/Keyboard.java
@@ -0,0 +1,4 @@
+package MyBot;
+
+public class Keyboard {
+}
