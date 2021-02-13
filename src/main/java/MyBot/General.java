b3c8ce8f711a5e32ac564f33558cd9ecfe2eaf6b
Walle95 <valentinshabanov95@gmail.com>
2021-02-13 23:36:35 +0700

    Fixed bug

--- src/main/java/MyBot/General.java
+++ src/main/java/MyBot/General.java
@@ -28,88 +28,16 @@ public class General extends TelegramWebhookBot {
     Emodji emodji = new Emodji();
     TextManager textManager = new TextManager();
 
+
     @Override
     public BotApiMethod onWebhookUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            // Set variables
-            String message_text = update.getMessage().getText();
-            long chat_id = update.getMessage().getChatId();
-
-
-            if (message_text.equals("/start")) {
-                SendMessage message = new SendMessage() // Create a message object object
-                        .setChatId(chat_id)
-                        .setText(textManager.text7);
-                SendPhoto sendPhoto = new SendPhoto().setChatId(chat_id);
-                try {
-                    sendPhoto.setPhoto("https://i.pinimg.com/736x/37/78/c3/3778c399e77ebe77f309b8bdf7be4e05.jpg");
-                    execute(sendPhoto);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-
-                // Создаем клавиатуру
-                ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-                //Меняем размер кнопок
-                keyboardMarkup.setResizeKeyboard(true);
-                // Создаем массив кнопок
-                List<KeyboardRow> keyboard = new ArrayList<>();
-
-                // Create a keyboard row
-                KeyboardRow row = new KeyboardRow();
-
-                //ассортимент
-                row.add(textManager.text6);
-               // Add the first row to the keyboard
-                keyboard.add(row);
-
-                //условия доставки
-                row = new KeyboardRow();
-                row.add(textManager.text2);
-                keyboard.add(row);
-
-                //связаться с нами
-                row = new KeyboardRow();
-                row.add(textManager.text1);
-                keyboard.add(row);
-
-                // Set the keyboard to the markup
-                keyboardMarkup.setKeyboard(keyboard);
-                // Add it to the message
-                message.setReplyMarkup(keyboardMarkup);
-
-                try {
-                    execute(message); // Sending our message object to user
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (message_text.equals(textManager.text1))
-            {
-                    SendMessage message2 = new SendMessage().setChatId(chat_id).setText(textManager.text4 + "\n\n"
-                            + emodji.emodjiPhone + "Phone:" + " 8-913-763-24-84" + "\n"
-                            + emodji.emodjiEmail + "Email:" + " valentinshabanov95@gmail.com");
-                try {
-                    execute(message2); // Sending our message object to user
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (message_text.equals(textManager.text2))
-            {
-                SendMessage message3 = new SendMessage().setChatId(chat_id).setText(textManager.text5);
-                try {
-                    execute(message3); // Sending our message object to user
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-            }
-
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChatId().toString());
+            sendMessage.setText("Well, all information looks like noise until you break the code.");
+            return sendMessage;
         }
-        return  null;
+        return null;
     }
 
     @Override
